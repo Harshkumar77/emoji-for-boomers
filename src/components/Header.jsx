005c18ef934dfd45da298902d37a610ec767c0ac
mighty_volcano <harshkumar@noreply.com>
2021-12-02 23:20:03 +0530

    Added basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>😀 Emoji for boomers</h1>
+      <p>Understand the meaning behind emoji used by genZ or Zoomers</p>
+    </div>
+  );
+}
+
+export default Header;
